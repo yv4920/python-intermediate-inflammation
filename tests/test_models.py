09c6c9d92d1e09cb482f67ce8538f91de3a6678c
Isaac Wells <isaac.wells@ukaea.uk>
2024-10-08 15:47:03 +0100

    test_models_update

--- tests/test_models.py
+++ tests/test_models.py
@@ -3,32 +3,18 @@
 import numpy as np
 import numpy.testing as npt
 import os
+import pytest
 
-
-def test_daily_mean_zeros():
-    """Test that mean function works for an array of zeros."""
+@pytest.mark.parametrize(
+        "test, expected",
+        [
+            ([ [0,0], [0,0], [0,0] ], [0,0]),
+            ([ [1,2], [3,4], [5,6] ], [3,4]),
+        ])
+def test_daily_mean(test, expected):
+    """Test mean function works for array of zeroes and positive integers."""
     from inflammation.models import daily_mean
-
-    test_input = np.array([[0, 0],
-                           [0, 0],
-                           [0, 0]])
-    test_result = np.array([0, 0])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
-
-
-def test_daily_mean_integers():
-    """Test that mean function works for an array of positive integers."""
-    from inflammation.models import daily_mean
-
-    test_input = np.array([[1, 2],
-                           [3, 4],
-                           [5, 6]])
-    test_result = np.array([3, 4])
-
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_mean(test_input), test_result)
+    npt.assert_array_equal(daily_mean(np.array(test)), np.array(expected))
 
 def test_load_from_json(tmpdir):
     from inflammation.models import load_json
@@ -38,46 +24,37 @@ def test_load_from_json(tmpdir):
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
 
-def test_daily_max_integers():
-    """Test that the max function works for an array of integers"""
+
+@pytest.mark.parametrize(
+        "test, expected",
+        [
+            ([ [0,0], [0,0], [0,0] ], [0,0] ),
+            ([ [10, 21], [32, 14], [15, 62] ], [32, 62]),
+            ([ [10.4, 21.112], [32.101, 114.5], [15.9, 6.082] ], [32.101, 114.5])
+        ])
+def test_daily_max(test, expected):
+    """Test max function works for array of positive integers and floats."""
     from inflammation.models import daily_max
+    npt.assert_array_equal(daily_max(np.array(test)), np.array(expected))
 
-    test_input = np.array([[10, 21],
-                          [32, 14],
-                          [15, 62]])
-    test_result = np.array([32, 62])
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
 
-def test_daily_max_floats():
-    """Test that the max function works for an array of integers"""
-    from inflammation.models import daily_max
+@pytest.mark.parametrize(
+        "test, expected",
+        [
+            ([ [0,0], [0,0], [0,0] ], [0,0] ),
+            ([ [10, 21], [32, 14], [15, 62] ], [10, 14]),
+            ([ [10.4, 21.112], [32.101, 114.5], [15.9, 6.082] ], [10.4, 6.082])
+        ])
 
-    test_input = np.array([[10.4, 21.112],
-                          [32.101, 114.5],
-                          [15.9, 6.082]])
-    test_result = np.array([32.101, 114.5])
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_max(test_input), test_result)
+def test_daily_min(test, expected):
+    """Test max function works for array of positive integers and floats."""
+    from inflammation.models import daily_min
+    npt.assert_array_equal(daily_min(np.array(test)), np.array(expected))
 
-def test_daily_min_integers():
-    """Test that the max function works for an array of integers"""
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
     from inflammation.models import daily_min
 
-    test_input = np.array([[10, 21],
-                          [32, 14],
-                          [15, 62]])
-    test_result = np.array([10, 14])
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
-
-def test_daily_min_floats():
-    """Test that the max function works for an array of integers"""
-    from inflammation.models import daily_min
-
-    test_input = np.array([[10.4, 21.112],
-                          [32.101, 114.5],
-                          [15.9, 6.082]])
-    test_result = np.array([10.4, 6.082])
-    # Need to use Numpy testing functions to compare arrays
-    npt.assert_array_equal(daily_min(test_input), test_result)
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
