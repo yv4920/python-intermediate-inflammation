e908d3cc1f3fd541eeca78e71ab79d1f1de1833c
Isaac Wells <isaac.wells@ukaea.uk>
2024-10-28 15:38:02 +0000

    Renamed standard dev func

--- inflammation/models.py
+++ inflammation/models.py
@@ -55,7 +55,7 @@ def daily_min(data):
     return np.min(data, axis=0)
 
 
-def s_dev(data):
+def standard_deviation(data):
     """Computes and returns standard deviation for data"""
 
     standard_dev_values = np.std(data)
